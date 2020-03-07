f16a359bbd48f311f1c67e758722f9a28bc92852
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-03-07 17:22:10 +0300

    feature: add morse decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,72 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+  // write your solution here
+    let len = 0;
+    len = expr.length;
+    //return len;
+    let res = '';
+    let count = 0;
+    let sum = 0;
+    let decodeRes = '';
+    count = len / 10;
+    let liter = '';
+    let k = 0;
+    //return count;
+   // return expr;
+    for (let i = 0; i < len; i++) {
+    	/*if (expr[i] == '*') {
+    		//sum = sum +1;
+            //return sum;
+            return i
+        }*/
+    	if(expr[i] == 1 && expr[i+1] == 0) {
+        //return i;
+        	res = res + '.';
+            ///return res;
+            i = i + 1;
+        } else if(expr[i] == 1 && expr[i+1] == 1) {
+        	res = res + '-';
+            i = i + 1;
+    	} else if (expr[i] == '*') {
+        	
+            //return i;
+            res = res + ' ';
+            i = i + 9;
+        } else if (expr[i] == 0 && expr[i+1] == 0) {
+        	
+            //return i;
+            res = res + '';
+            i = i + 1;
+        }
+        if (i % 10 ==9){
+        	res = res + '|';
+        }
+        //return res;
+        //return decodeRes;
+    }
+    len = res.length;
+    for(let j = 0; j < len; j++) {
+        	k = res.indexOf("|", j);
+            liter = res.substring(j,k);
+            j = k;
+        for (var key='' in MORSE_TABLE) {
+    		if (key == liter) {
+         		decodeRes = decodeRes + MORSE_TABLE[liter];
+        	}
+            
+        //return decodeRes;
+    	}
+        if (liter == ' ') {
+            	decodeRes = decodeRes + ' ';
+            }
+     }
+	return decodeRes;
+    //return sum;
+    return res; //Morse
+    //return decodeRes;
 }
 
 module.exports = {
